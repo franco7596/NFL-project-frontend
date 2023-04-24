c295e6f4632d38f113c4640f9a74cc5300aa0db4
franco7596 <franco_7_5_96@hotmail.com>
2023-04-24 20:26:16 -0300

    chanching the baack url

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-	baseURL: "http://127.0.0.1:3300/",
+	baseURL: "https://nflbackend.fly.dev/",
+	// baseURL: "http://127.0.0.1:3300/",
 });
 
 export default clienteAxios;
